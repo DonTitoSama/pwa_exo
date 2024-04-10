ad2a6b1cb1256868079b210107bc5eacf3274ec8
DontitoSama <jokonito06@gmail.com>
2024-04-10 11:33:48 +0200

    :pencil: Add project pwa with a API, offline page & cache files

---
+++ js/form.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Écoute à la validation (bouton rechercher) du formulaire
+    document.getElementById('searchForm').addEventListener('submit', function(event) {
+
+        event.preventDefault(); // empêche la soumission du formulaire
+
+        var searchTerm = document.getElementById('searchInput').value;
+
+        // Si le champs est vide ou contient seulement un espace, afficher une alerte
+        if (!searchTerm || searchTerm.trim().length === 0) {
+            alert("Veuillez saisir une valeur.");
+            return;
+        }         
+
+        addToSearchHistory(searchTerm);
+
+        fetch('https://api.tvmaze.com/singlesearch/shows?q=' + searchTerm)
+            .then(response => response.json())
+            .then(data => {
+                displaySearchResults(data);
+            })
+            .catch(error => {
+                console.error('Erreur dans la requête : ', error);
+            });
+    });
+
+
+    // Affiche les informations
+    function displaySearchResults(data) {
+        var resultsContainer = document.getElementById('searchResults');
+        resultsContainer.innerHTML = '';
+        var showName = data.name;
+        var showSummary = data.summary;
+        var showImage = data.image ? data.image.medium : 'Image non disponible.';
+    
+        var showDiv = document.createElement('div');
+        showDiv.classList.add('card');
+        showDiv.innerHTML = `
+            <div class="card-body" style="text-align: center;">
+                <h5 class="card-title" style="font-size: smaller;">${showName}</h5>
+                <a href="https://api.tvmaze.com/singlesearch/shows?q=${showName}" target="_blank">
+                    <img id="image" src="${showImage}" alt="${showName}" class="card-img-top" style="max-width: 200px; margin: auto; cursor: pointer;" title="Accéder au json">
+                </a>
+                <p class="card-text" style="font-size: smaller;">${showSummary ? showSummary : 'No summary available'}</p>
+            </div>`;
+        resultsContainer.appendChild(showDiv);
+    }
+
+
+
+    // Fonction pour ajouter un terme de recherche à l'historique
+    function addToSearchHistory(searchTerm) {
+        var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+        // Vérifier si le terme de recherche existe déjà dans l'historique
+        if (!searchHistory.includes(searchTerm)) {
+            // Ajouter le terme de recherche à l'historique
+            searchHistory.push(searchTerm);
+            // Mettre à jour le localStorage avec le nouvel historique
+            localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+        }
+    }
+});
---
+++ service_worker.js
@@ -0,0 +1,68 @@
+const VERSION = "v14";
+const HOST = location.protocol + '//' + location.host;
+const FILECACHE = [
+    HOST + "/css/bootstrap.css",
+    HOST + "/js/offline_form.js",
+];
+
+self.addEventListener("install", (e) => {
+    self.skipWaiting();
+    console.log("Version:", VERSION);
+
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(VERSION);
+
+            try {
+                await Promise.all(
+                    [...FILECACHE, './offline.html'].map(async (path) => {
+                        try {
+                            await cache.add(path);
+                            console.log("Cached:", path);
+                        } catch (error) {
+                            console.error("Cache add error:", error);
+                        }
+                    })
+                );
+            } catch (error) {
+                console.error("Cache open error:", error);
+            }
+        })()
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        (async () => {
+            const keys = await caches.keys();
+            await Promise.all(
+                keys.map((k) => {
+                    if (!k.includes(VERSION)) return caches.delete(k);
+                })
+            );
+        })()
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    console.log("Fetch:", e.request.url);
+
+    if (e.request.mode === "navigate") {
+        e.respondWith(
+            (async () => {
+                try {
+                    const preloadedResponse = await e.preloadResponse;
+                    if (preloadedResponse) return preloadedResponse;
+
+                    return await fetch(e.request);
+                } catch (error) {
+                    console.error("Fetch error:", error);
+                    const cache = await caches.open(VERSION);
+                    return await cache.match("offline.html");
+                }
+            })()
+        );
+    } else if (FILECACHE.includes(e.request.url)) {
+        e.respondWith(caches.match(e.request));
+    }
+});
